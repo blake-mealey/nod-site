34f0a7960aa83094e5b0cb0c5b52168c41efb722
timothy.mealey <timothy.mealey@ucalgary.ca>
2017-11-17 14:08:18 -0700

    Fix launch, add pages, start splash

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    // TODO: Redirect to mynotes if signed in
+    res.render('splash', {});
+});
+
+router.get('/signup', function(req, res, next) {
+    res.render('signup', {});
+});
+
+router.get('/mynotes', function(req, res, next) {
+    res.render('mynotes', {});
+});
+
+router.get('/note', function(req, res, next) {
+    res.render('note', {});
+});
+
+router.get('/settings', function(req, res, next) {
+    res.render('settings', {});
 });
 
 module.exports = router;
