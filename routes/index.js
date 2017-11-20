ea8960607e0c5c73b4c22e3f96cef49ff21a1836
Blake Mealey <blakemealey@gmail.com>
2017-11-20 07:02:05 +0000

    worked on summarizer

--- routes/index.js
+++ routes/index.js
@@ -132,41 +132,28 @@ router.get('/note', requiresLogin, function(req, res, next) {
     }
 });
 
-router.get('/temps/:file', function(req, res, next) {
-    return res.sendFile(path.resolve(path.join('temps', req.params.file)));
+router.get('/sentences/:noteId', function(req, res, next) {
+    Note.findById(req.params.noteId, function(err, note) {
+        if (err) return next(err);
+        var text = note.sentences.join(' ');
+        var text = "A news article discusses current or recent news of either general interest (i.e. daily newspapers) or of a specific topic (i.e. political or trade news magazines, club newsletters, or technology news websites).A news article can include accounts of eyewitnesses to the happening event. It can contain photographs, accounts, statistics, graphs, recollections, interviews, polls, debates on the topic, etc. Headlines can be used to focus the reader’s attention on a particular (or main) part of the article. The writer can also give facts and detailed information following answers to general questions like who, what, when, where, why and how.Quoted references can also be helpful. References to people can also be made through the written accounts of interviews and debates confirming the factuality of the writer’s information and the reliability of his source. The writer can use redirection to ensure that the reader keeps reading the article and to draw her attention to other articles. For example, phrases like Continued on page 3 redirect the reader to a page where the article is continued.While a good conclusion is an important ingredient for newspaper articles, the immediacy of a deadline environment means that copy editing often takes the form of deleting everything past an arbitrary point in the story corresponding to the dictates of available space on a page. Therefore, newspaper reporters are trained to write in inverted pyramid style, with all the most important information in the first paragraph or two. If the less vital details are pushed towards the end of the story, then the potentially destructive impact of draconian copy editing will be minimized."
+        var html = '<!DOCTYPE html><html><head><title>Note Contents</title></head><body><p>' + text + '</p></body></html>';
+        return res.send(html);
+    });
 });
 
 router.post('/notes/summarize', requiresLogin, enableSmmryCors, function (req, res, next) {
-
     if (req.body.id) {
-        Note.findById(req.body.id, function(err, note) {
+        var fileUrl = 'http://nod.chimerical.ca/sentences/' + req.body.id;
+        var url = 'http://api.smmry.com/&SM_API_KEY=944CDDA147&SM_LENGTH=5&SM_URL=' + fileUrl;
+        request(url, function(err, response) {
+            console.log(response.body)
             if (err) return res.send({ ok: false, err: err });
-            var text = note.sentences.join(' ');
-            fs.writeFile('../temps/' + req.body.id, text, function(err) {
-                if(err) return res.send({ ok: false, err: err });
-                var url = 'http://api.smmry.com/&SM_API_KEY=944CDDA147&SM_LENGTH=5&SM_URL=http://nod.chimerical.ca/temps/' + req.body.id;
-                request(url, function(err, response) {
-                    if (err) return res.send({ ok: false, err: err });
-                    return res.send({ ok: true, summary: response.body });
-                });
-            }); 
+            return res.send({ ok: true, summary: response.body });
         });
     } else {
         return res.send({ ok: false, err: 'missing_params' });
     }
-
-	// request("http://api.smmry.com/&SM_API_KEY=944CDDA147&SM_LENGTH=5", {
-	// 	sm_api_input: "This article is about the central reason for the RAF’s Coastal Command’s success in protecting British trade from German U-boats during the Second World War. Though Buckley does not mention any titles by name, he states that it is generally accepted by modern works on the Battle of the Atlantic that technology and operational research were key to Britain’s victory. However, Buckley’s thesis states that this is not the case, that technology and operational research were not the most influential factors in determining this victory. By technology he is referring to improvements in air-to-submarine weaponry, longer range radar, and improvements at spotting at night (such as the Leigh-light). By operational research he is referring to the improved usage of this technology to increase its effectiveness. Buckley attributes Coastal Commands success during the war mainly to the quantity and long range ability of the aircraft under its control. He argues that the most effective weapon that an aircraft possess is the so called “scarecrow” effect (P. 149), whereby when a U-boat spots an aircraft, it submerges to hide its location. Once submerged, U-boats could not travel quickly or far, so they would not be able to track or attack their intended target. Buckley uses a good mix of Primary and Secondary sources, spending the majority of the article arguing his case during the 1940-1941 Atlantic campaign, which ended with the British reducing their shipping loses by more than half (P. 145). During this campaign the British had outdated aircraft which were ill-equipped for submarine warfare, both in detection capabilities and armament. New technology and operation research had yet to make any real marks on the effectiveness of Coastal Command. Yet submarine attacks were foiled by these aircraft simply being present above and around conveys (P. 149). He furthers his point with statistics from the First World War showing how few surface vessels were attacked while being escorted by air. Anti-submarine technology from the air was practically non-existent during the First World War, so the “scarecrow” effect must be the cause. He also addresses the second Atlantic campaign from autumn 1942 to spring 1943, where the U-boats mostly operated in the so called Atlantic air gap. The U-boats were eventually driven out of the mid-Atlantic at the same time Coastal Command received the planes necessary to close the air gap. Buckley does concede that Coastal Commands ability to sink U-boats in large quantities during and after mid 1942 was a result of technology and operational research (P. 156), however the main goal of protecting shipping had already been achieved before any substantial number of U-boats had been sunk by aircraft."
-	// }, function (error, response, body) {
-	// 	if (error) {
-	// 		console.log('error:', error); // Print the error if one occurred
-	// 		return res.send({ ok: false});
-	// 	} else {
-	// 		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	// 		console.log('body:', body); // Print the HTML for the Google homepage.
-	// 		return res.send({ ok: true, summary: "Blueberries!!"});
-	// 	}
-	// });
 });
 
 /* GET request for account settings page */
